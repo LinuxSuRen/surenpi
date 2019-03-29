271d872ab954e0890e737e26a47eb064fd6da3df
Zhao Xiaojie <zxjlwt@126.com>
2019-03-29 14:52:39 +0800

    Add static resources

---
+++ static/push/config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAANFe_A1M:APA91bEQJZk0GsKFrt9xcb8NRQiVZ--p12b3QZHj4gSsFD6rTtQxXW65gHLSIgxChhDC4RIVPxHC_kEw2egp9hwiKFA9gFCyXgvsAKjbFCF9ffJaC41NkpvmNNviLmqeaIIdmE__jlJS'
+};
